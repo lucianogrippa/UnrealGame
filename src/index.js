23d138ec6362d3c01372b792e82565a79f6093fb
lucianogrippa <grippa.luciano@gmail.com>
2022-03-08 21:32:48 +0100

    organized folder workspaces

--- src/index.js
+++
@@ -1,5 +0,0 @@
-import "core-js/stable";
-import "regenerator-runtime/runtime";
-
-"...and this JS message should be seen in the browser console!" |> console?.log;
-
--- webpack.config.js
+++
@@ -1,3 +0,0 @@
-// This file exists so that it is possible to run `npx webpack` from the command line.
-
-module.exports = require("./webpack.development.config");
