f70c0054398d204e5b014d18593c144f7ea2ee38
Luciano <grippa.luciano@gmail.com>
2022-03-08 21:51:36 +0100

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,5 @@
+import "core-js/stable";
+import "regenerator-runtime/runtime";
+
+"...and this JS message should be seen in the browser console!" |> console?.log;
+
---
+++ webpack.config.js
@@ -0,0 +1,3 @@
+// This file exists so that it is possible to run `npx webpack` from the command line.
+
+module.exports = require("./webpack.development.config");
